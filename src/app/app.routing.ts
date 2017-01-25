d190ab48589522908ab1f501193788e31b0ade11
Yevhenii Kravchenko <yevhenii.kravchenko@gmail.com>
2017-01-25 20:41:32 +0200

    Added search module

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -2,7 +2,8 @@ import { Routes, RouterModule } from '@angular/router';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'tracker', pathMatch: 'full' },
-  { path: 'tracker', loadChildren: 'app/tracker/tracker.module#TrackerModule' }
+  { path: 'tracker', loadChildren: 'app/tracker/tracker.module#TrackerModule' },
+  { path: 'search', loadChildren: 'app/search/search.module#SearchModule' }
 ];
 
 export const routing = RouterModule.forRoot(routes);
